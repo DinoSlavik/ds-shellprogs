05bb79d253e393f9584050cccf4ab0ff2dbd3893
DinoSlavik <82023888+DinoSlavik@users.noreply.github.com>
2022-12-22 19:44:46 +0200

    Update updater.sh

--- ds-updatesys/updater.sh
+++ ds-updatesys/updater.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-read -p "Use dist-upgrade? (y/n) " dist_upgrade
+read -p "Use dist-upgrade? (y/n) " dist_upgrade    # Create a variable that tells if a user wants to use dist-upgrade
 
-if [ $dist_upgrade = 'y' ] || [ $dist_upgrade = 'Y' ]; then
+if [ $dist_upgrade = 'y' ] || [ $dist_upgrade = 'Y' ]; then    # If a user wants to use dist-upgrade use it
     sudo apt update
     sudo apt upgrade -y
     sudo apt dist-upgrade -y
-elif [ $dist_upgrade = 'n' ] || [ $dist_upgrade = 'N' ]; then
+elif [ $dist_upgrade = 'n' ] || [ $dist_upgrade = 'N' ]; then    # Else use a basic upgrade
     sudo apt update
     sudo apt upgrade -y
-else
+else    # If pressed other key exit from the program
     exit
 fi
