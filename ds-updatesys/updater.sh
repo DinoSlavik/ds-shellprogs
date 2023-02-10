1d13e682e6e552292ddcca7b573c247ed1a860cf
Dino Slavik <dino.slavik@gmail.com>
2023-02-10 15:14:34 +0200

    Added basic parametres for dist-upgrade and full-upgrade

--- ds-updatesys/updater.sh
+++ ds-updatesys/updater.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-read -p "Use dist-upgrade? (y/n) " dist_upgrade    # Create a variable that tells if a user wants to use dist-upgrade
+sudo apt update    # Update repos
 
-if [ $dist_upgrade = 'y' ] || [ $dist_upgrade = 'Y' ]; then    # If a user wants to use dist-upgrade use it
-    sudo apt update
-    sudo apt upgrade -y
-    sudo apt dist-upgrade -y
-elif [ $dist_upgrade = 'n' ] || [ $dist_upgrade = 'N' ]; then    # Else use a basic upgrade
-    sudo apt update
-    sudo apt upgrade -y
-else    # If pressed other key exit from the program
-    exit
+if [ ${args[0]} = 'dist' ]; then    # If uses dist parametr perform disro upgrade
+    sudo apt dist-upgrade
+elif [ ${args[0]} = 'full' ]; then    # If uses dist parametr perform full upgrade
+    sudo apt full-upgrade
+else    # Else perform default upgrade
+    sudo apt upgrade
 fi
