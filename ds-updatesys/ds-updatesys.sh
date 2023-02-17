2c1d987f06f596d9454dfb09d65558c6417e9df7
Dino Slavik <dino.slavik@gmail.com>
2023-02-17 19:25:45 +0200

    Changed arguments to options (not work actually)

--- ds-updatesys/ds-updatesys.sh
+++ ds-updatesys/ds-updatesys.sh
@@ -1,11 +1,58 @@
 #!/bin/bash
 
-sudo apt update    # Update repos
+############################################################
+# Help                                                     #
+############################################################
+Help() {
+   # Display Help
+   echo "Many apt commands in one command"
+   echo
+   echo "Syntax: ds-updatesys [-d|f|h|]"
+   echo "options:"
+   echo "h     Print this Help."
+   echo "d     Update system and distro version"
+   echo "f     Perform full upgrade version"
+   echo
+}
 
-if [ ${args[0]} = 'dist' ]; then    # If uses dist parameter perform disro upgrade
-    sudo apt dist-upgrade
-elif [ ${args[0]} = 'full' ]; then    # If uses dist parameter perform full upgrade
-    sudo apt full-upgrade
-else    # Else perform default upgrade
-    sudo apt upgrade
-fi
+############################################################
+############################################################
+# Program                                                  #
+############################################################
+############################################################
+
+############################################################
+# Functions                                                #
+############################################################
+
+DefaultUpdate() {
+    sudo apt update    # Update repos
+    sudo apt upgrade   # Else perform default upgrade
+}
+
+FullUpdate() {
+    sudo apt update       # Update repos
+    sudo apt full-upgrade # If uses dist parameter perform full upgrade
+}
+
+DistroUpdate() {
+    sudo apt update       # Update repos
+    sudo apt dist-upgrade # If uses dist parameter perform disro upgrade
+}
+
+############################################################
+# Main Code                                                #
+############################################################
+
+while getopts ":h:d:f:" options; do
+    case $options in
+        h)  # Display Help
+            Help ;;
+        d)  # Distro update
+            DistroUpdate ;;
+        f)  # Full update
+            FullUpdate ;;
+        #*)  #Default update
+        #    DefaultUpdate ;;
+    esac
+done
