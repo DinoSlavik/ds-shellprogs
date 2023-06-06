cc61c5806e103e20c75906f96f563196282b278c
DinoSlavik <82023888+DinoSlavik@users.noreply.github.com>
2023-06-06 18:00:14 +0300

    Added auto apt update at startup
    1. Added auto update repos at startup   using INIT_SYSTEM variable
    2. Changed "if [ ]" to "if[[ ]]" for more usability in develop

--- ds-updatesys/updater.sh
+++ ds-updatesys/updater.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-sudo apt update    # Update repos
+if [[ "$INIT_SYSTEM" == "startup" ]]; then # Check is startup now with INIT_SYSTEM variable
+    apt-get -q=2 -y update # If startup - update repos
+else # Else - use common program
 
-if [ ${args[0]} = 'dist' ]; then    # If uses dist parametr perform disro upgrade
-    sudo apt dist-upgrade
-elif [ ${args[0]} = 'full' ]; then    # If uses dist parametr perform full upgrade
-    sudo apt full-upgrade
-else    # Else perform default upgrade
-    sudo apt upgrade
+    sudo apt update    # Update repos
+
+    if [[ ${args[0]} == 'dist' ]]; then    # If uses dist parametr perform disro upgrade
+        sudo apt dist-upgrade
+    elif [[ ${args[0]} == 'full' ]]; then    # If uses dist parametr perform full upgrade
+        sudo apt full-upgrade
+    else    # Else perform default upgrade
+        sudo apt upgrade
+    fi
 fi
+
+exit 0
